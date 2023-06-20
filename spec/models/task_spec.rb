86c71920392616f08c050a6d3f1ecb39bc888c61
yuta <calendula6724yuta@gmail.com>
2023-06-20 13:22:50 +0900

    modelテストOK

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -20,4 +20,33 @@ RSpec.describe "タスクモデル機能", type: :model do
       end
     end
   end
+  describe "検索機能" do
+    # 必要に応じて、テストデータの内容を変更して構わない
+    let!(:task) { FactoryBot.create(:task, title: "task") }
+    let!(:second_task) { FactoryBot.create(:second_task, title: "sample") }
+    context "scopeメソッドでタイトルのあいまい検索をした場合" do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
+        expect(Task.title_search("task")).to include(task)
+        expect(Task.title_search("task")).not_to include(second_task)
+        expect(Task.title_search("task").count).to eq 1
+      end
+    end
+    context "scopeメソッドでステータス検索をした場合" do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.where(status: "未着手")).to include(task)
+        expect(Task.where(status: "未着手")).not_to include(second_task)
+        expect(Task.where(status: "未着手").count).to eq 1
+      end
+    end
+    context "scopeメソッドでタイトルのあいまい検索とステータス検索をした場合" do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        expect(Task.title_search("t").where(status: "未着手")).to include(task)
+        expect(Task.title_search("t").where(status: "未着手")).not_to include(
+          second_task,
+        )
+        expect(Task.title_search("t").where(status: "未着手").count).to eq 1
+      end
+    end
+  end
 end
