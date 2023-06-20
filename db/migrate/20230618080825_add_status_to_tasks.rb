0e2dc4caa7260d8a38d9046ff57d429d0f3d127a
yuta <calendula6724yuta@gmail.com>
2023-06-20 10:01:37 +0900

    status検索完了

--- db/migrate/20230618080825_add_status_to_tasks.rb
+++ db/migrate/20230618080825_add_status_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddStatusToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_column :tasks, :status, :string, null: false, default:0
+    add_column :tasks, :status, :integer, null: false, default:0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_18_080825) do
+ActiveRecord::Schema.define(version: 2023_06_20_004140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2023_06_18_080825) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "end_time", null: false
-    t.string "status", default: "0", null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
