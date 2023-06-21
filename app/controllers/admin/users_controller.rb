1cfabad1c3943c24f4dea3dd9b65f3a17923b4e0
yuta <calendula6724yuta@gmail.com>
2023-06-21 17:55:01 +0900

    adminカラム作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,12 +3,6 @@ class Admin::UsersController < ApplicationController
     @users = User.all.includes(:tasks) #n1問題
   end
 
-  def new
-    respond_to do |format|
-format.html # This line was already here.
-end
-  end
-
   def create
     @user = User.new(user_pramas)
     if @user.save
@@ -17,4 +11,42 @@ end
       render :new
     end
   end
+
+  def new
+    @user = User.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "情報をへんしゅうしました"
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "ユーザーを削除しました"
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+  end
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_21_054337) do
+ActiveRecord::Schema.define(version: 2023_06_21_085343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2023_06_21_054337) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
