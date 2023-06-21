8c1a014dc15bab17706c5523c33c2a293e713985
yuta <calendula6724yuta@gmail.com>
2023-06-21 13:32:01 +0900

    user madel作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_20_042934) do
+ActiveRecord::Schema.define(version: 2023_06_21_042555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2023_06_20_042934) do
     t.datetime "end_time", null: false
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 0, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
