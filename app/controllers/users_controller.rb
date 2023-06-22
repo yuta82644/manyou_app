560017ae66f918470cfac788d682daa6690983f5
yuta <calendula6724yuta@gmail.com>
2023-06-22 15:45:35 +0900

    テストOK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to tasks_path
+      redirect_to user_path(@user.id)
     else
       render :new
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "normaluser" }
+    email { "normal@dic.com" }
+    password { "111111" }
+    password_confirmation { "111111" }
+    admin {false}
+  end
+
+  factory :admin_user, class: User do
+    name { "adminuser" }
+    email { "admin@dic.com" }
+    password { "222222" }
+    password_confirmation { "222222" }
+    admin {true}
   end
 end
+
+#一般と管理者のデータ作る
