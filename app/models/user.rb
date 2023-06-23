63faef379481ca377c7028c02aeedeac25422fe1
yuta <calendula6724yuta@gmail.com>
2023-06-23 10:55:24 +0900

    コールバック実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_update :admin_update_exist
+  before_destroy :admin_exist_check
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :email,
             presence: true,
@@ -12,4 +15,19 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 6 }
   has_many :tasks, dependent: :destroy
+
+  private
+
+  def admin_update_exist
+    if User.where(admin: true).count == 1 && self.admin == false
+    throw(:abort)
+  end
 end
+
+  def admin_exist_check
+    if User.where(admin: true).count <= 1 && self.admin == true
+      throw(:abort)
+    end
+  end
+end
+
